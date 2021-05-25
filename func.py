e72c518e6463eb0b3aea5930d8a800b29298fadf
Weiting <54346415+weiting1029@users.noreply.github.com>
2021-05-25 15:05:35 +0200

    update with a more efficient routine to determine lambda

--- func.py
+++ func.py
@@ -1,7 +1,7 @@
 import numpy as np
 from numpy.random import multivariate_normal
 from numpy.random import normal
-from sklearn.linear_model import LassoCV
+from sklearn.linear_model import LassoCV, Lasso
 from sklearn.linear_model import LassoLarsCV
 from scipy import stats
 from matplotlib import pyplot
@@ -70,14 +70,35 @@ def test_power_simulation_desf(seed, KK, alpha, theta_candidate, mu, sigma, mean
             Y_temp = ERR_temp + Q_temp @ hyp_beta
             reg_temp = LassoCV(cv=5, random_state=0).fit(Q_temp, Y_temp)
             temp_beta = reg_temp.coef_
-            lambda_list[j] = lambda_list[j]+reg_temp.alpha_
+            lambda_list[j] = lambda_list[j] + reg_temp.alpha_
             #             print(temp_beta[0])
             u_score = u_score_function(temp_beta, Y_temp, Q_temp, sigma)
             p_value = 1 - 2 * (1 - stats.norm.cdf(abs(u_score)))
             if p_value > 1 - alpha:
                 rej_list[j] += 1
-    lambda_list = 
-    return rej_list
+    lambda_list = lambda_list / 50
+
+    for i in range(50, KK):
+        np.random.seed(seed + i)
+        ERR_temp = normal(mu, sigma, n)
+        #         print(random.random())
+        Q_temp = multivariate_normal(mean=mean1, cov=Sigma, size=n)
+        p = len(mean1)
+        hyp_beta = np.zeros(p)
+        hyp_beta[1:3] = 1
+        for j in range(len(theta_candidate)):
+            hyp_beta[0] = theta_candidate[j]
+            Y_temp = ERR_temp + Q_temp @ hyp_beta
+            reg_temp = Lasso(alpha=lambda_list[j]).fit(Q_temp, Y_temp)
+            temp_beta = reg_temp.coef_
+            # lambda_list[j] = lambda_list[j] + reg_temp.alpha_
+            #             print(temp_beta[0])
+            u_score = u_score_function(temp_beta, Y_temp, Q_temp, sigma)
+            p_value = 1 - 2 * (1 - stats.norm.cdf(abs(u_score)))
+            if p_value > 1 - alpha:
+                rej_list[j] += 1
+
+    return rej_list / KK
 
 
 def estimate_Sigma_inverse(Q):
@@ -156,7 +177,7 @@ def test_power_simulation_ldpe(seed, KK, alpha, theta_candidate, mu, sigma, mean
     return rej_list
 
 
-def find_KK(seed, alpha, can_KK,  mu, sigma, mean1, Sigma, n):
+def find_KK(seed, alpha, can_KK, mu, sigma, mean1, Sigma, n):
     #     beta[0]=0
     rej_list = np.zeros(len(can_KK))
     for i in range(len(can_KK)):
